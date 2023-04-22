229b1331853033b2a2d3123150a0f4b5c5a6686f
repomanu <97444718+manu-tuli@users.noreply.github.com>
2023-04-22 21:55:53 +0200

    resume_1.6

--- js/observer.js
+++ js/observer.js
@@ -1,21 +1,36 @@
 
 
 // intersection observer
-const elmts = document.querySelectorAll(".btm-layer")
-const observer = new IntersectionObserver((entries)=>{
+const images = document.querySelectorAll(".btm-layer")
+const texts = document.querySelectorAll("aside")
+const imageObserver = new IntersectionObserver((entries)=>{
     for (const entry of entries) {
         if (entry.isIntersecting) {
             entry.target.classList.add("show")
             // console.log(entry.target.classList)
         } else {
-            // entry.target.classList.remove("show")
+            entry.target.classList.remove("show")
+            // console.log(entry.target.classList)
+        }
+    }
+},{threshold: 0.5})
+const textObserver = new IntersectionObserver((entries)=>{
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("reveal")
+            // console.log(entry.target.classList)
+        } else {
+            entry.target.classList.remove("reveal")
             // console.log(entry.target.classList)
         }
     }
 },{threshold: 0.5})
 
-for (let elmt of elmts) {
-    observer.observe(elmt)
+for (let elmt of images) {
+    imageObserver.observe(elmt)
+}
+for (let elmt of texts) {
+    textObserver.observe(elmt)
 }
 
 // observer.disconnect()
